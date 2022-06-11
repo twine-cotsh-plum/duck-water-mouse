2fdef9221db60a1381939588bb2b78844dba4e08
twine-cotsh-plum <107318275+twine-cotsh-plum@users.noreply.github.com>
2022-06-11 21:37:59 +0300

    Dep-220611093757

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 var express = require("express");
 var app = express();
 
+// Create GET request
+app.get('/', (req, res) => {
+  res.send('Express on Vercel')
+})
+
+// Initialize server
+app.listen(5000, () => {
+  console.log('Running on port 5000.')
+})
+
 var metadataRouter = require('./routes/api/token-metadata')
 app.use('/api/token-metadata', metadataRouter);
 
